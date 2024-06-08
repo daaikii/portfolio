82b9da1b9c987e4796925eb7e80c67ecb78c9542
daiki <ikeda0210daiki@ezweb.ne.jp>
2024-06-08 13:27:50 +0900

    correction

--- src/glsl/planeVertex.glsl
+++ src/glsl/planeVertex.glsl
@@ -2,8 +2,7 @@ varying vec2 vUv;
 
 void main(){
  vUv = uv;
+  vec4 mvPosition = modelViewMatrix * vec4(position,1.0);
 
-  // vec4 mvPosition = modelViewMatrix * vec4(position,1.0);
-
-  gl_Position =vec4(position,1.0);
+  gl_Position =projectionMatrix*mvPosition;
 }
